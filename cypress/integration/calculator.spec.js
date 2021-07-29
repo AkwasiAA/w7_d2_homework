743c09aa80e9136600567a29e4e03510b76d0e83
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-07-29 19:15:07 +0100

    fourth integration test complete

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -24,4 +24,18 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '40');
   })
+
+  it('should be able to chain multiple operations together', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number8').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '5')
+  })
 })
